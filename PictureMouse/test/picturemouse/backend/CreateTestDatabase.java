cc36d763e1368fdf561b54ce1d04c17278b51535
Jake Carr <c.jake@live.co.uk>
2014-04-29 16:24:53 +0100

    Trying to add another film to the database

--- PictureMouse/test/picturemouse/backend/CreateTestDatabase.java
+++ PictureMouse/test/picturemouse/backend/CreateTestDatabase.java
@@ -33,14 +33,32 @@ public class CreateTestDatabase {
         // Create Films
         // Create Screening
         ArrayList<BEScreening> testScreenings1 = new ArrayList<>();
-        HashMap<Integer, String> testSeatingPlan = new HashMap<>();
-        testSeatingPlan.put(1, "testUser1");
-        testSeatingPlan.put(2, "Administrator");
-        BEScreening testScreening1 = new BEScreening(1, new Time(9, 30, 0), new Date(114, 5, 1), 1, testSeatingPlan);
+        HashMap<Integer, String> testSeatingPlan1 = new HashMap<>();
+        testSeatingPlan1.put(1, "testUser1");
+        testSeatingPlan1.put(2, "Administrator");
+        BEScreening testScreening1 = new BEScreening(1, new Time(9, 30, 0), new Date(114, 5, 1), 1, testSeatingPlan1);
         testScreenings1.add(testScreening1);
 
         BEFilm testFilm1 = new BEFilm(1, "Test Film 1", "", "Test Synopsis 1", testScreenings1);
         database.save(testFilm1);
+        
+        // Create Screening
+        ArrayList<BEScreening> testScreenings2 = new ArrayList<>();
+        HashMap<Integer, String> testSeatingPlan2 = new HashMap<>();
+        testSeatingPlan2.put(5, "testUser1");
+        testSeatingPlan2.put(11, "Administrator");
+        BEScreening testScreening2 = new BEScreening(2, new Time(9, 30, 0), new Date(114, 5, 2), 1, testSeatingPlan2);
+        testScreenings2.add(testScreening2);
+
+        ArrayList<BEScreening> testScreenings3 = new ArrayList<>();
+        HashMap<Integer, String> testSeatingPlan3 = new HashMap<>();
+        testSeatingPlan3.put(23, "testUser1");
+        testSeatingPlan3.put(1, "Administrator");
+        BEScreening testScreening3 = new BEScreening(3, new Time(11, 30, 0), new Date(114, 5, 2), 1, testSeatingPlan3);
+        testScreenings2.add(testScreening3);
+        
+        BEFilm testFilm2 = new BEFilm(2, "Test Film 2", "", "Test Synopsis 2", testScreenings2);
+        database.save(testFilm2);
 
 
 
